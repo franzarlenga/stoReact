98e7c3ce0cdb0d8cedf570b66f4c692c637454ca
Francisco Zarlenga <franzarlenga6@gmail.com>
2022-08-23 22:50:53 -0300

    fixing

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import "../style/styles.css";
+
+const Navbar = () => {
+return (
+    <header>
+        <nav className="navbar">
+            <ul>
+                <li className="li-items">
+                    <a href="#">Menu</a>
+                </li>
+            </ul>
+            <ul>
+                <li className="li-items">
+                    <a href="#">Menu</a>
+                </li>
+            </ul>
+            <ul>
+                <li className="li-items">
+                    <a href="#">Menu</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+);
+}
+
+export default Navbar;
